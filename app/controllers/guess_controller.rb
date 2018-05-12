0106d0acffd74f5c89ac0106874163509a20aecc
Matt Heard <matt@mattheard.net>
2018-05-12 21:00:58 +0200

    Respond with OK 200 if a guess is given

--- app/controllers/guess_controller.rb
+++ app/controllers/guess_controller.rb
@@ -1,11 +1,19 @@
 class GuessController < ApplicationController
   def evaluate
-    set_bad_response_response_code 
+    set_response_status
   end
 
   private
 
-  def set_bad_response_response_code
-    head :bad_request
+  def set_response_status
+    head(guess_given? ? :ok : :bad_request)
+  end
+
+  def guess_given?
+    guess
+  end
+
+  def guess
+    params[:guess]
   end
 end
--- spec/requests/guesses_spec.rb
+++ spec/requests/guesses_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe 'making guesses', type: :request do
   describe 'GET /guess' do
     context 'with no parameters' do
-      it 'fails' do
+      it "returns a 'bad request' response" do
         get '/guess'
         expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'with a valid guess and no state' do
+      it "returns an 'ok' response" do
+        get '/guess?guess=1'
+        expect(response).to have_http_status(:ok)
       end
     end
   end
